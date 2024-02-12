d9ea9c113542bd740bb8d375b5683236955be360
Ssurafels <ssurafel4@gmail.com>
2024-02-12 16:33:20 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :reservations, dependent :distroy
+         has_many :reservations, dependent :destroy
          validates :username, presence: true, uniqueness: true
 
 end
