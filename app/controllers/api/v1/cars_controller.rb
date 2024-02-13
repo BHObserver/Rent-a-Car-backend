59d9c8c8b17e40a9a9a44035f658d73d330b25b3
BHObserver <BHObserver0@gmail.com>
2024-02-13 17:04:48 +0600

    add item

---
+++ app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,48 @@
+# app/controllers/api/v1/cars_controller.rb
+class Api::V1::CarsController < ApplicationController
+  # GET /cars
+  def index
+    @cars = Car.all
+    render json: { cars: @cars }
+  end
+
+  # GET /cars/1
+  def show
+    @car = Car.find(params[:id])
+    render json: @car
+  end
+
+  # POST /cars
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      render json: @car, status: :created
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /cars/1
+  def destroy
+    @car = Car.find(params[:id])
+
+    if @car.destroy
+      render json: { message: 'Car deleted successfully' }, status: :no_content
+    else
+      render json: { error: 'Failed to delete car' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def car_params
+    params.require(:car).permit(:name, :model, :image, :description, :number_of_seats, :location, :fee, :reserved)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
@@ -7,6 +6,7 @@ Rails.application.routes.draw do
       delete '/logout', to: 'sessions#destroy'
       resources :users
       resources :reservations
+      resources :cars, only: [:index, :create, :destroy] # Add this line
     end
   end
 end
