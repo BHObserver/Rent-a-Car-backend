33cb8420dd44cfcd7db778d981aa3c0e8198d7e6
BHObserver <BHObserver0@gmail.com>
2024-02-13 18:09:54 +0600

    Update cars_controller.rb

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -43,6 +43,6 @@ class Api::V1::CarsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def car_params
-    params.require(:car).permit(:name, :model, :image, :description, :number_of_seats, :location, :fee, :reserved)
+    params.require(:car).permit(:name, :model, :image, :description, :number_of_seats, :location, :fee, :reserved, :user_id)
   end
 end
