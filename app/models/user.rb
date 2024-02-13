8de0d226c9a02367e6fd8b14d903cfd5cbb69b64
BHObserver <BHObserver0@gmail.com>
2024-02-14 02:49:59 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,4 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 end
