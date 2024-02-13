3fcd787eb5500862c51f6a6ee0a28965d31b3a8a
BHObserver <BHObserver0@gmail.com>
2024-02-13 16:45:01 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
-# Rails.application.routes.draw do
-#   devise_for :users
-#   resources :users
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-#   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-#   # Can be used by load balancers and uptime monitors to verify that the app is live.
-#   get "up" => "rails/health#show", as: :rails_health_check
-
-#   # Defines the root path route ("/")
-#   # root "posts#index"
-# end
 Rails.application.routes.draw do
   namespace :api do
-    post '/register', to: 'registration#create'
-    post '/login', to: 'session#create'
-    delete '/logout', to: 'session#destroy'
+    namespace :v1 do
+      post '/register', to: 'registrations#create'
+      post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+      resources :users
+      resources :reservations
+    end
   end
-end 
+end
