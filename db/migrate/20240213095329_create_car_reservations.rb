888d7775d9d0b40a31a7e12f73ffa5022a2a876d
BHObserver <BHObserver0@gmail.com>
2024-02-13 16:45:05 +0600

    Create 20240213095329_create_car_reservations.rb

---
+++ db/migrate/20240213095329_create_car_reservations.rb
@@ -0,0 +1,15 @@
+class CreateCarReservations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :car_reservations do |t|
+      t.date :reserved_date
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :start_location
+      t.string :destination
+      t.references :user, null: false, foreign_key: true
+      t.references :car, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
