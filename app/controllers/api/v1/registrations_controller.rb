f337e19f7b1c89327ccfef2acb1cee631c462081
Ssurafels <ssurafel4@gmail.com>
2024-02-14 15:15:16 +0300

    Fix conflict errors

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:app/controllers/api/v1/registration_controller.rb
-class Api::V1::RegistrationController < ApplicationController
-========
 class Api::V1::RegistrationsController < ApplicationController
->>>>>>>> feature/reservation:app/controllers/api/v1/registrations_controller.rb
   def create
     @user = User.new(user_params)
 
