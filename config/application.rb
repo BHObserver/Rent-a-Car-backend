0a03d81847692042f0d40a02c539d03eca95856e
BHObserver <BHObserver0@gmail.com>
2024-02-16 01:38:39 +0600

    update files

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module RentACarBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.api_only = true
+    # config.api_only = true
 
   end
 end
