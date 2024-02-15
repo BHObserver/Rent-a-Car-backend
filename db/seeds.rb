e2b546c76f94df933ecab00b14bcedb5bb1249f1
Ssurafels <ssurafel4@gmail.com>
2024-02-15 20:25:13 +0300

    Add seed Cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,31 @@ User.find_or_create_by!(email: 'admin@example.com') do |user|
     user.password = 'password'
     user.password_confirmation = 'password'
   end
+  
+# Seed Cars
+Car.find_or_create_by!(name: 'Tesla Model S') do |car|
+  car.photo = 'tesla_model_s.jpg'
+  car.details = 'Electric car with advanced features'
+  car.city = 'San Francisco'
+  car.model = 'Model S'
+  car.cost = 100
+  car.user = admin
+end
+
+Car.find_or_create_by!(name: 'BMW 3 Series') do |car|
+  car.photo = 'bmw_3_series.jpg'
+  car.details = 'Luxury sedan with excellent performance'
+  car.city = 'Los Angeles'
+  car.model = '3 Series'
+  car.cost = 120
+  car.user = user
+end
+
+Car.find_or_create_by!(name: 'Toyota Camry') do |car|
+  car.photo = 'toyota_camry.jpg'
+  car.details = 'Reliable and fuel-efficient family sedan'
+  car.city = 'New York'
+  car.model = 'Camry'
+  car.cost = 80
+  car.user = admin
+end
