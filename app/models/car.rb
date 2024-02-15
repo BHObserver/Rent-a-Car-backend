e78a204fc5264cd617f0d067bda1bc32f5071ab8
Ssurafels <ssurafel4@gmail.com>
2024-02-15 14:15:46 +0300

    Validates car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,12 @@
-# app/models/car.rb
 class Car < ApplicationRecord
   belongs_to :user
   has_many :car_reservations
+
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :details, presence: true
+  validates :city, presence: true
+  validates :model, presence: true
+  validates :cost, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
 end
