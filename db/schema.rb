40af3657d93903d46dd9a0ed0804c2ee245cd3c1
BHObserver <BHObserver0@gmail.com>
2024-02-20 12:55:01 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_095329) do
     t.string "model"
     t.integer "year"
     t.boolean "available", default: true
+    t.string "photo_url"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
