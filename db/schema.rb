29ed998bdd10709d7ff734f38d189351b3eef303
BHObserver <BHObserver0@gmail.com>
2024-02-22 03:58:03 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_095329) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_125329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
